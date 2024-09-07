68a1ed368538e9258b8e60543eddc3e02f2bf2ac
Devon Friend <devon.friend45@gmail.com>
2024-09-06 21:14:47 -0700

    Change tests to use abstracted API calls

--- dht/tests/single_node_tests.rs
+++ dht/tests/single_node_tests.rs
@@ -2,13 +2,11 @@
 
 use ctor::ctor;
 use lazy_static::lazy_static;
-use std::os::linux::raw::stat;
 use std::path::Path;
 use std::net::SocketAddr;
 
 use dht::util::read_socket_addresses;
-use dht::comm::proto::{Operation, Status, extract_reply};
-use dht::comm::protogen::api::{Request, Reply};
+use dht::comm::proto::Status;
 
 mod common;
 
@@ -50,43 +48,35 @@ fn test03_Get_KeyNotFound() {
     let key: Vec<u8> = common::get_rand_key();
 
     let (_, status) = common::get_value(*SERVER_ADDR, &key).unwrap();
-    assert_eq!(reply.status, Status::KeyNotFound as u32);
+    assert_eq!(status, Status::KeyNotFound as u32);
 
     let _ = common::wipe_servers(vec![*SERVER_ADDR], 1);
 }
 
 #[test]
-fn test04_Wipe_Success() {
+fn test04_Put_Get_Wipe_Success() {
     const NUM_KEY_VALUE_PAIRS: usize = 10;
     let _result = common::ping_servers(vec![*SERVER_ADDR], true);
-    let proto_interface = common::get_proto_interface().unwrap();
 
     let mut key_value_pairs: Vec<(Vec<u8>, Vec<u8>)> = Vec::new();
     for _ in 0..NUM_KEY_VALUE_PAIRS {
-        key_value_pairs.push((common::get_rand_key(), common::get_rand_value()));
-    }
+        let (key, value, status) = common::put_rand_key_value(*SERVER_ADDR).unwrap();
+        assert_eq!(status, Status::Success as u32);
 
-    for (key, value) in key_value_pairs.iter() {
-        let mut request: Request = Request::new();
-        request.operation = Operation::Put as u32;
-        request.key = Some(key.to_vec());
-        request.value = Some(value.to_vec());
+        let (retrived_value_opt, status) = common::get_value(*SERVER_ADDR, &key).unwrap();
+        assert_eq!(status, Status::Success as u32);
 
-        let (reply_msg, _server_socket) = proto_interface.send_and_recv(request, *SERVER_ADDR).unwrap();
-        let reply: Reply = extract_reply(&reply_msg).unwrap();
-        assert_eq!(reply.status, Status::Success as u32);
+        let retrived_value: Vec<u8> = retrived_value_opt.ok_or("GET failed. No value present").unwrap();
+        assert_eq!(retrived_value, value);
+
+        key_value_pairs.push((key, value));
     }
 
     let result = common::wipe_servers(vec![*SERVER_ADDR], 1);
     assert!(result.is_ok());
 
     for (key, _) in key_value_pairs.iter() {
-        let mut request: Request = Request::new();
-        request.operation = Operation::Get as u32;
-        request.key = Some(key.to_vec());
-
-        let (reply_msg, _server_socket) = proto_interface.send_and_recv(request, *SERVER_ADDR).unwrap();
-        let reply: Reply = extract_reply(&reply_msg).unwrap();
-        assert_eq!(reply.status, Status::KeyNotFound as u32);
+        let (_, status) = common::get_value(*SERVER_ADDR, key).unwrap();
+        assert_eq!(status, Status::KeyNotFound as u32);
     }
 }

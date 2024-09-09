8c00dc4463ef90f67a304ebc68d35cc6fa20c52f
Devon Friend <devon.friend45@gmail.com>
2024-09-08 17:48:30 -0700

    Ask for sudo permissions at the beginning of the test script

--- dht/scripts/run_tests.sh
+++ dht/scripts/run_tests.sh
@@ -3,6 +3,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_ARGS="--show-output --test-threads 1"
 export RUST_BACKTRACE=1
 
+# Ask for sudo permissions now to avoid asking later after the tests have finished
+# since stop_servers.sh requires sudo permissions
+sudo -v
+
 cargo test --test test_single_node_is_alive -- ${TEST_ARGS} && \
     cargo test --test test_single_node_basic_operations -- ${TEST_ARGS}
 

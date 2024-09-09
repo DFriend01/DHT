078439f50b8d1ec9f9cd284a886679f6bc7ab532
Devon Friend <devon.friend45@gmail.com>
2024-09-08 17:52:02 -0700

    Update stop_servers script

--- dht/scripts/stop_servers.sh
+++ dht/scripts/stop_servers.sh
@@ -4,7 +4,7 @@ binary_name="dht"
 server_pids=$(sudo netstat -tunlp | tail -n +3 | awk '{print $6}' | grep ${binary_name} | cut -d '/' -f1)
 
 if [ -z "${server_pids}" ]; then
-    echo "No servers running"
+    echo "No servers found"
 else
     nservers=$(echo -e ${server_pids} | wc -w)
     echo "Stopping ${nservers} servers with PIDs: ${server_pids}"

068fe8ab7b5cee0ce33971cc9be7b2abcb7ffeec
Devon Friend <devon.friend45@gmail.com>
2025-09-20 20:15:49 -0700

    Change integration test script to use new util scripts

--- dht/scripts/run_integration_tests.sh
+++ dht/scripts/run_integration_tests.sh
@@ -3,9 +3,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_ARGS="--show-output --test-threads 1"
 export RUST_BACKTRACE=1
 
+NUM_NODES="1"
+SERVER_LIST_FILE="servers/single_node.txt"
+
 # Ask for sudo permissions now to avoid asking later after the tests have finished
 # since stop_servers.sh requires sudo permissions
 sudo -v
+
+${SCRIPT_DIR}/util/run_servers.sh "${NUM_NODES}" "${SERVER_LIST_FILE}"
 
 cargo test --test test_single_node_is_alive -- ${TEST_ARGS} && \
     cargo test --test test_single_node_basic_operations -- ${TEST_ARGS} && \
@@ -15,5 +20,5 @@ cargo test --test test_single_node_shutdown -- ${TEST_ARGS}
 
 if [ $? -ne 0 ]; then
     echo "WARNING: The shutdown test failed. Killing server processes..."
-    ${SCRIPT_DIR}/stop_servers.sh
+    ${SCRIPT_DIR}/util/stop_servers.sh
 fi
--- dht/tests/tests_prelude/mod.rs
+++ dht/tests/tests_prelude/mod.rs
@@ -5,7 +5,7 @@ use lazy_static::lazy_static;
 use std::path::Path;
 use dht::util::read_socket_addresses;
 
-const SERVER_FILE: &str = "servers/single_server.txt";
+const SERVER_FILE: &str = "servers/single_node.txt";
 lazy_static! {
     pub static ref SERVER_ADDR: SocketAddr = read_socket_addresses(Path::new(SERVER_FILE)).unwrap()[0];
 }

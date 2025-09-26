8772665279a1aedc8aa2c2151a6e97ab6cd96602
Devon Friend <devon.friend45@gmail.com>
2025-09-25 23:02:00 -0700

    Implement MD5 hash helper

--- dht/src/util/mod.rs
+++ dht/src/util/mod.rs
@@ -1,7 +1,11 @@
+use std::array::TryFromSliceError;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::net::SocketAddr;
 use std::path::Path;
+
+use md5::{Md5, Digest};
+use md5::digest::core_api::CoreWrapper;
 
 pub fn read_socket_addresses(server_file_path: &Path) -> Result<Vec<SocketAddr>, io::Error> {
     let file: File = File::open(server_file_path)?;
@@ -23,12 +27,24 @@ pub fn read_socket_addresses(server_file_path: &Path) -> Result<Vec<SocketAddr>,
     Ok(addresses)
 }
 
+pub fn hash_md5(data: impl AsRef<[u8]>) -> Result<[u8; 16], TryFromSliceError> {
+    let mut hasher: CoreWrapper<md5::Md5Core> = Md5::new();
+    hasher.update(data);
+    let hash = hasher.finalize();
+    let owned_hash: [u8; 16] = match hash.as_slice().try_into() {
+        Ok(owned_hash) => owned_hash,
+        Err(e) => return Err(e)
+    };
+    Ok(owned_hash)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fs::remove_file;
     use std::io::Write;
     use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+    use hex_literal::hex;
 
     #[test]
     fn test_read_socket_addresses() {
@@ -78,5 +94,13 @@ mod tests {
         remove_file(test_file_path).unwrap();
 
         assert_eq!(result, addrs);
+    }
+
+    #[test]
+    fn test_hash_md5() {
+        // Compared against output from https://www.md5hashgenerator.com/
+        let expected_hash: [u8; 16] = hex!("2bd8f6bcb0f4bc5dd2d1a4844344f11d");
+        let actual_hash: [u8; 16] = hash_md5(b"Pineapple belongs on pizza").unwrap();
+        assert_eq!(actual_hash, expected_hash);
     }
 }
